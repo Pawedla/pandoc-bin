9635f65930de1885ed94627475968105fabcac09
Peter <61778415+Pawedla@users.noreply.github.com>
2021-02-28 15:29:22 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#loading functions
+# loading functions
 . $(dirname "$0")/functions.sh
 
 # setting up python environment
@@ -11,13 +11,13 @@ source venv/bin/activate
 # requirements can be added that are needed for pandoc filters
 pip install -r ./base/.pandoc/requirements.txt --no-binary :all:
 
-#loading env settings
+# loading env settings
 yml_to_env "$(get_settings "settingsCompile" "./settingsGlobal.yml")" | sed "s/.*\[.*\].*//" > debug.env
 source base/bin/base.env
 source debug.env
 export DEBUG
 
-#generate Documents according to loaded settings (Type: automatic book, manual book, single source; format: pdf, docx)
+# generate Documents according to loaded settings (Type: automatic book, manual book, single source; format: pdf, docx)
 if [[ ${OUTPUT_FORMAT_PDF} = true ]] ; then
     [[ ${CREATE_AUTOMATIC_BOOKS} = true ]] && get_automatic_books | xargs -d '\n' -I{} -n1 -P${THREADS} /bin/bash -c './base/bin/make-files.sh --a --pdf --source "{}" '
     [[ ${CREATE_MANUAL_BOOKS} = true ]] && get_manual_book_source | xargs -d '\n' -I{} -n1 -P${THREADS} /bin/bash -c './base/bin/make-files.sh --m --pdf --source "{}" '
