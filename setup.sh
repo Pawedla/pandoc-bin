d9222815ae1570e44785cb303dee850edecc72c2
Peter <61778415+Pawedla@users.noreply.github.com>
2021-02-28 15:15:28 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,6 +10,8 @@ printf "%100s\n" |tr " " "="
 BASE_DIR="$(git rev-parse --show-toplevel)" # working directory of the docker image (/usr/share/blog)
 BIN_DIR=${BASE_DIR}/base/bin # relative path to the scripts directory (./bin)
 WORKING_DIR=$(dirname "${SOURCE}") # relative direcrory of the current source file
+
+# Changing working dir, if source is not a file but directory
 [[ ${CREATION_TYPE} = manual || ${CREATION_TYPE} = automatic ]] && WORKING_DIR=${SOURCE}
 
 [[ ${DEBUG} = true ]] && printf "%-40s%s\n" "[BASE_DIR]:" ${BASE_DIR}
